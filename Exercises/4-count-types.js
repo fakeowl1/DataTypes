fc6d9b228ba95e1e820f43b4a1e0d2e08f8240e7
hok7z <hok7zv@tutanota.com>
2024-11-27 15:18:02 +0200

    First commit

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,15 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = (ArrayOfTypes) => {
+  const countTypes = {};
+  for (const item of ArrayOfTypes) {
+    const type = typeof item;
+    let count = countTypes[type];
+    count = count ? count++ : count = 0;
+    countTypes[type] = count;
+  };
+
+  return countTypes;
+};
 
 module.exports = { countTypesInArray };
